9c2c5f9798c44e91bc3eb024e153db12745162ea
Danilo Santos <danilo.santos0404@hotmail.com>
2022-02-28 06:09:21 -0300

    Event handleInputNumberChange added

--- projeto-dsviacep/js/controllers/form-controler.js
+++ projeto-dsviacep/js/controllers/form-controler.js
@@ -1,7 +1,6 @@
-import Address from '../models/address.js';
+import Address from "../models/address.js";
 
 function State() {
-
   this.address = new Address();
 
   this.btnSave = null;
@@ -30,5 +29,18 @@ export function init() {
   state.errorCep = document.querySelector('[data-error="cep"]');
   state.errorNumber = document.querySelector('[data-error="number"]');
 
-  console.log(state);
+  state.inputNumber.addEventListener('change', handleInputNumberChange);
+}
+
+function handleInputNumberChange(event) {
+    if(event.target.value == "") {
+        setFormError("number", "Campo requirido");
+    } else {
+        setFormError("number", "");
+    }
+}
+
+function setFormError(key, value) {
+  const element = document.querySelector(`[data-error="${key}"]`);
+  element.innerHTML = value;
 }
