4022b5d92a40fc895f4c8601bb9a5c3c70f5488d
Danilo Santos <danilo.santos0404@hotmail.com>
2022-03-07 06:08:53 -0300

    added error

--- projeto-dsviacep/js/controllers/form-controler.js
+++ projeto-dsviacep/js/controllers/form-controler.js
@@ -61,9 +61,26 @@ async function handleInputCepChange(event) {
   }
 }
 
-async function handleBtnSaveClick(event) {
+function handleBtnSaveClick(event) {
   event.preventDefault();
-  listController.addCard(state.address);
+
+  const errors = AddressService.getErrors(state.address);
+  const keys = Object.keys(errors);
+
+  if(keys.length > 0) {
+
+    keys.forEach(key =>{
+      setFormError(key, errors[key]);
+    });
+
+    //for(let i = 0; i < keys.length; i++) {
+    //  setFormError(keys[i], errors[keys[i]]);
+    //}
+  }
+  else {
+    listController.addCard(state.address);
+    clearForm();
+  }  
 }
 
 function handleInputNumberChange(event) {
@@ -87,6 +104,8 @@ function clearForm() {
 
   setFormError("cep", "");
   setFormError("number", "");
+
+  state.address = new Address();
 
   state.inputCep.focus();
 }
